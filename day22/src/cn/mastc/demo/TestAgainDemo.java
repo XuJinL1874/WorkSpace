0cdf555d0b885ac1b8c54259e31cc3bb01370961
XuJinL1874 <164833192@qq.com>
2018-08-06 17:49:45 +0800

    test

---
+++ day22/src/cn/mastc/demo/TestAgainDemo.java
@@ -0,0 +1,23 @@
+package cn.mastc.demo;
+
+/**
+ * @Author: XuJin_L
+ * @Description:again测试用idea把项目上传到自己的GitHub仓库
+ * @Date: Created in 16:59 2018/8/6
+ * @Modified By:
+ */
+public class TestAgainDemo {
+    public static void main(String[] args) {
+        function();
+    }
+
+    public static void function() {
+        int a = 1;
+        int b = 1;
+        if (a == b) {
+            System.out.println("TestAgain上传GitHub仓库成功!");
+        }else{
+            System.out.println("TestAgain上传GitHub仓库失败!");
+        }
+    }
+}
