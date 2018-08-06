53754d7954cad83aaffcdc0c103555a9b77dc429
XuJinL1874 <164833192@qq.com>
2018-08-06 18:33:17 +0800

    test

---
+++ day22/src/cn/mastc/demo/HelloWorld.java
@@ -0,0 +1,13 @@
+package cn.mastc.demo;
+
+/**
+ * @Author: XuJin_L
+ * @Description:new world come it!
+ * @Date: Created in 18:30 2018/8/6
+ * @Modified By:
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("new world come it!");
+    }
+}
