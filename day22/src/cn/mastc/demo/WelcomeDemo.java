7b569d3c3220d0c207c69fb117cbe51de8b29976
XuJinL1874 <164833192@qq.com>
2018-08-06 16:34:22 +0800

    Initial commit

---
+++ day22/src/cn/mastc/demo/WelcomeDemo.java
@@ -0,0 +1,25 @@
+package cn.mastc.demo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ */
+public class WelcomeDemo {
+    public static void main(String[] args) {
+        function();
+        function_1();
+    }
+    public static void function_1(){
+        ArrayList<String> array = new ArrayList<String>();
+        System.out.println("Welcome to IDEA!");
+        System.out.println("这是第一个IDEA写的Java程序!");
+    }
+    public static void function(){
+        for (int i = 0; i <10; i++) {
+
+        }
+        System.out.println("IDEA快捷键!!");
+        System.out.println("使用IDEA快捷键!!");
+    }
+}
